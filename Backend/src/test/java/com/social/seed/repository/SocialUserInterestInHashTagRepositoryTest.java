14b3e1c8eca4947fa58263ee380d19b80275f34e
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2023-12-29 13:48:38 -0600

    first iteration of testing

---
+++ Backend/src/test/java/com/social/seed/repository/SocialUserInterestInHashTagRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.social.seed.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+class SocialUserInterestInHashTagRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void existsInterest() {
+    }
+
+    @Test
+    void addInterest() {
+    }
+
+    @Test
+    void deleteInterest() {
+    }
+}
