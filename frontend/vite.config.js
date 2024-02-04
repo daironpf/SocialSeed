e6a34dc11e9b4497cda7a7d06f580e9b5e62b4c4
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2024-02-04 10:25:20 -0500

    Initialize Vue.js project
    Created a new Vue.js project using the Vue CLI. Initial dependencies and basic project structure have been set up.

---
+++ frontend/vite.config.js
@@ -0,0 +1,28 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  //change port for production
+  preview: {
+    port: 8080,
+  },
+  // for dev
+  server: {
+    port: 8080,
+  },
+
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  define: {
+    __APP_VERSION__: JSON.stringify('v0.0.1')
+  }
+})
