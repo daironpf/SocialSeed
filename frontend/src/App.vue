41870fba8f762a9f95e1fc4cdefdeb3bc5a5c69a
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2024-02-21 20:16:03 -0500

    add fontawesome

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,20 @@
-<template>
-  <h1>SocialSeed</h1>
+<template lang="html">
+  <div id="encabezado" class="w-full bg-blue-500 text-white py-4 fixed top-0 z-50">
+    <div class="container mx-auto flex justify-between items-center">
+      <div class="logo text-2xl font-bold">SocialSeed</div>
+      <nav class="menu">
+        <ul class="flex space-x-4">
+          <li><a href="#" class="hover:text-blue-300"><fa icon="fa-solid fa-home" /></a></li>
+          <li><a href="#" class="hover:text-blue-300"><fa icon="fa-solid fa-user" /></a></li>
+          <li><a href="#" class="hover:text-blue-300"><fa icon="fa-solid fa-bell" /></a></li>
+          <li><a href="#" class="hover:text-blue-300"><fa icon="fa-solid fa-envelope" /></a></li>
+          <li><a href="#" class="hover:text-blue-300"><fa icon="fa-solid fa-cog" /></a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+
+</script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+import {
+    faHome,
+    faUser,
+    faBell,
+    faEnvelope,
+    faCog
+} from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faHome, faUser, faBell, faEnvelope, faCog);
+
+createApp(App).component('fa', FontAwesomeIcon).mount('#app');
