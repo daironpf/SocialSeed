138f9980e0804d3435278d59d6f2c8713f637729
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2024-03-07 12:37:19 -0500

    Add Group to Join suggestions

--- frontend/src/views/FeedView.vue
+++ frontend/src/views/FeedView.vue
@@ -205,8 +205,8 @@
     <!-- Lateral Derecho -->
     <div class="basis-1/4 bg-gray-200">
 
+      <!--        Sugerencias de Amistad-->
       <div>
-        <!--        Sugerencias de Amistad-->
         <div class="flex flex-row justify-between">
 
           <p class="font-bold">Pedir Amistad</p>
@@ -423,6 +423,77 @@
         <a href="">Ver todas las recomendaciones</a>
       </div>
 
+      <!--        Sugerencias de Grupos a Unirse  -->
+      <div>
+        <div class="flex flex-row justify-between mt-5 b-2">
+
+          <p class="font-bold">Grupos a Unirse</p>
+          <p>
+            <fa icon="fa-solid fa-sync" class="text-gray-500 "/>
+          </p>
+        </div>
+        <!--      Sugerencia N -->
+        <div class="bg-gray-50 rounded-lg shadow ml-3 mr-3 p-2">
+          <!--        Zona de Recomendaciones para Amigos-->
+          <div class="flex flex-row">
+            <!--          Encabezado-->
+            <img src="/img/vegapunk.jfif" alt="Foto de usuario" class="w-12 h-12 rounded-full mr-4">
+            <div>
+              <div>
+                <p>Punk</p>
+                <span class="text-xs">Grupo</span>
+              </div>
+              <div class="flex justify-between pt-3">
+                <!-- Botonera -->
+                <div class="bg-sky-200">+</div>
+                <div class="ml-4 bg-sky-200"> C </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <!--      Sugerencia N-1 -->
+        <div class="bg-gray-50 rounded-lg shadow ml-3 mr-3 p-2 mt-2">
+          <!--        Zona de Recomendaciones para Amigos-->
+          <div class="flex flex-row">
+            <!--          Encabezado-->
+            <img src="/img/vegapunk.jfif" alt="Foto de usuario" class="w-12 h-12 rounded-full mr-4">
+            <div>
+              <div>
+                <p>Amantes de One Piece</p>
+                <span class="text-xs">Grupo</span>
+              </div>
+              <div class="flex pt-3">
+                <!-- Botonera -->
+                <div class="bg-sky-200">+</div>
+                <div class="ml-3 bg-sky-200"> C </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!--      Sugerencia N-2 -->
+        <div class="bg-gray-50 rounded-lg shadow ml-3 mr-3 p-2 mt-2">
+          <!--        Zona de Recomendaciones para Amigos-->
+          <div class="flex flex-row">
+            <!--          Encabezado-->
+            <img src="/img/tesla.png" alt="Foto de usuario" class="w-12 h-12 rounded-full mr-4">
+            <div>
+              <div>
+                <p>Grandes Cientificos</p>
+                <span class="text-xs">Grupo</span>
+              </div>
+              <div class="flex pt-3">
+                <!-- Botonera -->
+                <div class="bg-sky-200">+</div>
+                <div class="ml-3 bg-sky-200"> C </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <a href="">Ver todas las recomendaciones</a>
+      </div>
     </div>
 
   </div>
