d6176a1659acf8315a8a673284b5af75fb08d37c
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2024-03-07 12:21:31 -0500

    Add follows suggestions

--- frontend/src/views/FeedView.vue
+++ frontend/src/views/FeedView.vue
@@ -277,6 +277,81 @@
         <a href="">Ver todas las recomendaciones</a>
       </div>
 
+
+      <div>
+        <!--        Sugerencias de Usuarios a Seguir  -->
+        <div class="flex flex-row justify-between mt-5 b-2">
+
+          <p class="font-bold">Usuarios a Seguir</p>
+          <p>
+            <fa icon="fa-solid fa-sync" class="text-gray-500 "/>
+          </p>
+        </div>
+        <!--      Sugerencia N -->
+        <div class="bg-gray-50 rounded-lg shadow ml-3 mr-3 p-2">
+          <!--        Zona de Recomendaciones para Amigos-->
+          <div class="flex flex-row">
+            <!--          Encabezado-->
+            <img src="/img/vegapunk.jfif" alt="Foto de usuario" class="w-12 h-12 rounded-full mr-4">
+            <div>
+              <div>
+                <p>VegaPunk</p>
+                <span class="text-xs">SocialUser</span>
+              </div>
+              <div class="flex justify-between pt-3">
+                <!-- Botonera -->
+                <div class="bg-sky-200">Seguir</div>
+                <div class="ml-4 bg-gray-400">Te Sigue</div>
+                <div class="ml-4 bg-sky-200"> C </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <!--      Sugerencia N-1 -->
+        <div class="bg-gray-50 rounded-lg shadow ml-3 mr-3 p-2 mt-2">
+          <!--        Zona de Recomendaciones para Amigos-->
+          <div class="flex flex-row">
+            <!--          Encabezado-->
+            <img src="/img/vegapunk.jfif" alt="Foto de usuario" class="w-12 h-12 rounded-full mr-4">
+            <div>
+              <div>
+                <p>VegaPunk</p>
+                <span class="text-xs">SocialUser</span>
+              </div>
+              <div class="flex justify-between pt-3">
+                <!-- Botonera -->
+                <div class="bg-sky-200">Seguir</div>
+                <div class="ml-4 bg-sky-200"> C </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!--      Sugerencia N-2 -->
+        <div class="bg-gray-50 rounded-lg shadow ml-3 mr-3 p-2 mt-2">
+          <!--        Zona de Recomendaciones para Amigos-->
+          <div class="flex flex-row">
+            <!--          Encabezado-->
+            <img src="/img/tesla.png" alt="Foto de usuario" class="w-12 h-12 rounded-full mr-4">
+            <div>
+              <div>
+                <p>Nikola Tesla</p>
+                <span class="text-xs">SocialUser</span>
+              </div>
+              <div class="flex justify-between pt-3">
+                <!-- Botonera -->
+                <div class="bg-sky-200">Seguir</div>
+                <div class="ml-4 bg-gray-400">Te Sigue</div>
+                <div class="ml-4 bg-sky-200"> C </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <a href="">Ver todas las recomendaciones</a>
+      </div>
+
     </div>
 
   </div>
