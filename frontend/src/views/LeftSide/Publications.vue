1eb30b0240a58034583b82854b9ade71242e9bc0
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2024-03-11 15:57:16 -0500

    Add Publications to LeftSide

---
+++ frontend/src/views/LeftSide/Publications.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+</script>
+
+<template>
+  <!-- Publicaciones -->
+  <div class="rounded-lg shadow p-3 mt-4 bg-gray-50">
+    <div class="grid grid-cols-1 divide-y">
+      <div class="text-center font-bold">Publicaciones</div>
+      <div class="flex justify-between items-center">
+        <p class="font-bold">Amigos </p><span class="bg-blue-200 rounded-full px-2 text-white">30</span>
+      </div>
+      <div class="flex justify-between items-center">
+        <p class="font-bold">Seguidos </p><span class="bg-blue-200 rounded-full px-2 text-white">10</span>
+      </div>
+      <div class="flex justify-between items-center">
+        <p class="font-bold">Grupos </p><span class="bg-blue-200 rounded-full px-2 text-white">134</span>
+      </div>
+      <div class="flex justify-between items-center">
+        <p class="font-bold">Páginas </p><span class="bg-blue-200 rounded-full px-2 text-white">534</span>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
