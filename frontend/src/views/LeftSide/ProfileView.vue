6bac4367be6cd99123680434c335cb68eca9f8ed
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2024-03-11 15:56:59 -0500

    Add Profile to LeftSide

---
+++ frontend/src/views/LeftSide/ProfileView.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="rounded-lg shadow w-30 bg-gray-50">
+    <!--        Perfil-->
+    <div class="bg-cover  h-20 rounded-t-lg" style="background-image: url('/img/background-user-test.jpg')"></div>
+    <div class="p-4">
+      <img src="/img/user-perfil.jpg" class="rounded-full w-16 h-16 border border-white border-2 -mt-11 mx-auto" alt="">
+      <h1 class="text-xl font-bold text-color-black text-center mt-2">Dairon Pérez Frías</h1>
+      <p class="text-gray-700 text-center">Me encantan las mascotas, especialmente los perritos.</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
