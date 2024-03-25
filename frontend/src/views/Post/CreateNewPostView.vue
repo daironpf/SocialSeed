fdff5e0affabfccde9a56730894239b75bf589d8
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2024-03-25 17:21:56 -0500

    fixed create post modal window

--- frontend/src/views/Post/CreateNewPostView.vue
+++ frontend/src/views/Post/CreateNewPostView.vue
@@ -24,12 +24,26 @@
           </div>
           <!-- Contenido -->
           <div class="p-4 flex-grow">
-            <textarea class="w-full h-full border border-gray-300 rounded-md p-2" placeholder="Escribe tu publicación aquí..." @click.stop></textarea>
+            <textarea
+                v-model="postContent"
+                class="w-full h-full border border-gray-300 rounded-md p-2"
+                placeholder="Escribe tu publicación aquí..."
+                @click.stop>
+
+            </textarea>
           </div>
           <!-- Botones-->
           <div class="bg-gray-100 p-3 px-6 justify-end flex gap-4">
-            <button @click="hideDialog" class="bg-white border-[1px] border-gray-300 rounded px-4 py-2 text-black cursor-pointer hover:bg-gray-100">Cancelar</button>
-            <button class="bg-blue-400 rounded px-4 py-2 text-white cursor-pointer hover:bg-blue-600">Publicar</button>
+            <button
+                @click="hideDialog"
+                class="bg-white border-[1px] border-gray-300 rounded px-4 py-2 text-black cursor-pointer hover:bg-gray-100">
+              Cancelar
+            </button>
+            <button
+                @click="publishPost"
+                class="bg-blue-400 rounded px-4 py-2 text-white cursor-pointer hover:bg-blue-600">
+              Publicar
+            </button>
           </div>
         </div>
       </div>
@@ -51,13 +65,24 @@
       </div>
     </div>
   </div>
+
+  <!-- Ventana emergente de notificación -->
+  <div v-if="notificationVisible" class="fixed inset-x-0 bottom-0 z-50 flex justify-center mb-4">
+    <div class="bg-green-400 text-white p-4 rounded-md shadow-md">
+      {{ notificationMessage }}
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import {inject, ref} from "vue";
 
 const currentUser = ref(JSON.parse(localStorage.getItem('currentUser')));
 const isDialogOpen = ref(false);
+const postContent = ref('');
+const apiUrl = inject('apiUrl');
+const notificationVisible = ref(false);
+const notificationMessage = ref('');
 
 function showDialog() {
   isDialogOpen.value = true;
@@ -65,6 +90,46 @@ function showDialog() {
 
 function hideDialog() {
   isDialogOpen.value = false;
+}
+
+async function publishPost() {
+  try {
+    // Aquí supondré que tienes un endpoint /posts en tu API REST
+    const response = await fetch(apiUrl+'post/createPost',
+        {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                'userId': currentUser.value.id
+              },
+              body: JSON.stringify({
+                content: postContent.value,
+              }),
+        });
+
+    if (response.ok) {
+      // Si la publicación es exitosa, puedes realizar alguna acción, como cerrar el modal o mostrar un mensaje de éxito
+      hideDialog();
+      showNotification('Publicación exitosa');
+      console.log('Publicación exitosa');
+    } else {
+      // Si hay algún error en la publicación, puedes manejarlo aquí
+      showNotification('Error al publicar');
+      console.error('Error al publicar');
+    }
+  } catch (error) {
+    // Manejo de errores de red u otros errores
+    showNotification('Error al procesar la solicitud');
+    console.error('Error al procesar la solicitud:', error);
+  }
+}
+
+function showNotification(message) {
+  notificationMessage.value = message;
+  notificationVisible.value = true;
+  setTimeout(() => {
+    notificationVisible.value = false;
+  }, 5000); // La notificación desaparecerá después de 5 segundos
 }
 </script>
 
