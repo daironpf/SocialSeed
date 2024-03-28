c5d9634246307925e7fe4a43d64ea598146defb9
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2024-03-28 10:04:55 -0500

    add animate rotate spin to refresh recommendations

--- frontend/src/views/RightSide/FriendsRecommendationsLiteView.vue
+++ frontend/src/views/RightSide/FriendsRecommendationsLiteView.vue
@@ -1,52 +1,71 @@
 <script setup>
-import SocialUserRelationshipCard from "@/views/SocialUser/SocialUserToFriendCard.vue";
-import {inject, onMounted, ref} from "vue";
+
+import { ref, inject, onMounted } from "vue";
 import SocialUserToFriendCard from "@/views/SocialUser/SocialUserToFriendCard.vue";
 
 const currentUser = ref(JSON.parse(localStorage.getItem('currentUser')));
 const apiUrl = inject('apiUrl')
-
 const socialUsers = ref([]);
+const loading = ref(false);
 
 async function cargarDatos() {
   try {
+    loading.value = true;
     const response = await fetch(apiUrl + 'friend/friend-recommendations-lite/' + currentUser.value.id);
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
     const data = await response.json();
-
     socialUsers.value = data.response;
-    console.log('Recomendacion de Amigos', data)
-
-  } catch (e) {
-    console.error(e);
+    console.log('Recomendacion de Amigos', data);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    loading.value = false;
   }
 }
 
 onMounted(() => {
   cargarDatos();
 });
+
+async function recargarSugerencias() {
+  await cargarDatos();
+  console.log('por recargar sugerencia: ',loading.value)
+}
 </script>
 
 <template>
-  <!--        Sugerencias de Amistad-->
+  <!-- Sugerencias de Amistad -->
   <div>
-    <div class="flex flex-row justify-between">
-
+    <div class="flex flex-row justify-between items-center mb-4">
       <p class="font-bold">Pedir Amistad</p>
-      <p>
-        <fa icon="fa-solid fa-sync" class="text-gray-500 "/>
-      </p>
+      <button @click="recargarSugerencias" :disabled="loading" class="flex items-center focus:outline-none">
+        <fa icon="fa-solid fa-sync" class="text-gray-500" :class="{ 'animate-spin': loading }" />
+      </button>
     </div>
 
-    <SocialUserToFriendCard
-        v-for="user in socialUsers"
-        :user="user"
-        :key="user.id"
-    />
+    <!-- Lista de Sugerencias de Amistad -->
+    <div v-if="!loading">
+      <SocialUserToFriendCard
+          v-for="user in socialUsers"
+          :user="user"
+          :key="user.id"
+      />
+    </div>
+    <div v-else class="text-gray-600">Cargando...</div>
 
+    <!-- Ver todas las recomendaciones -->
     <div class="font-semibold text-gray-600">>> Ver todas las recomendaciones</div>
   </div>
 </template>
 
-<style scoped>
-
+<style>
+/* Estilos opcionales para animar el icono de carga */
+@keyframes spin {
+  to { transform: rotate(360deg); }
+}
+.animate-spin {
+  animation: spin 1s linear infinite;
+}
 </style>
