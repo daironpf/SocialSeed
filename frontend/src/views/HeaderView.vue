826e666060585e89eff1e5548b42cde8fe49d466
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2024-04-01 12:56:34 -0500

    Add link to SocialSeed logo

--- frontend/src/views/HeaderView.vue
+++ frontend/src/views/HeaderView.vue
@@ -5,7 +5,10 @@ import {RouterLink} from "vue-router";
 <template>
   <div id="encabezado" class="w-full bg-blue-500 text-white py-4 fixed top-0 z-50">
     <div class="container mx-auto flex justify-between items-center">
-      <div class="logo text-2xl font-bold">SocialSeed</div>
+      <RouterLink to="/">
+        <div class="logo text-2xl font-bold">SocialSeed</div>
+      </RouterLink>
+
       <nav class="menu">
         <ul class="flex space-x-4">
           <li><RouterLink to="/" class="hover:text-blue-300"><fa icon="fa-solid fa-home" /></RouterLink></li>
