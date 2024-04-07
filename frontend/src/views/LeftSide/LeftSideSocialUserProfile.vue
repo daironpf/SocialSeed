ff43d2b12022b96a3e3a6f321d12b0c966f52648
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2024-04-07 12:07:12 -0500

    working in su-profile

---
+++ frontend/src/views/LeftSide/LeftSideSocialUserProfile.vue
@@ -0,0 +1,25 @@
+<script setup>
+import ProfileView from "@/views/LeftSide/ProfileView.vue";
+import Preferences from "@/views/LeftSide/Preferences.vue";
+import Publications from "@/views/LeftSide/Publications.vue";
+
+const props = defineProps({
+  userId: String,
+})
+</script>
+
+<template>
+  <div class="basis-1/4  m-1 bg-gray-200">
+    <h1>{{userId}}</h1>
+    <ProfileView />
+
+    <Preferences />
+
+    <Publications />
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
