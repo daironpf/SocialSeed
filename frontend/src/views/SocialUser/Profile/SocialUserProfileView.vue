c7641620b78fd0470c150238dc4aba5c363d3e33
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2024-04-12 18:03:29 -0500

    working in Tooltip component

--- frontend/src/views/SocialUser/Profile/SocialUserProfileView.vue
+++ frontend/src/views/SocialUser/Profile/SocialUserProfileView.vue
@@ -7,13 +7,36 @@ import { useRoute } from 'vue-router'
 import LeftSideSocialUserProfile from "@/views/SocialUser/Profile/LeftSideSocialUserProfile.vue";
 import ListOfFollowRecommendations from "@/views/Lists/ListOfFollowRecommendations.vue";
 import ListOfFriendsRecommendations from "@/views/Lists/ListOfFriendsRecommendations.vue";
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
+import {arrow, computePosition, flip, offset, shift} from "@floating-ui/dom";
 
 const route = useRoute()
 const userId = route.params.id
 
 
+const referenceRef = ref();
+const floatingRef = ref();
+const arrowRef = ref();
 
+
+onMounted(async () => {
+  const {x, y, middlewareData, placement} = await computePosition(referenceRef.value, floatingRef.value, {
+    placement: "bottom",
+    middleware: [offset(8), flip(), shift({padding: 5}), arrow({element: arrowRef.value})]
+  });
+
+  Object.assign(floatingRef.value.style, {
+    left: `${x}px`,
+    top: `${y}px`,
+  });
+
+  const {x: arrowX, y: arrowY} = middlewareData.arrow;
+
+  Object.assign(arrowRef.value.style, {
+    left: arrowX ? `${arrowX}px`: "",
+    top: arrowY ? `${arrowY}px`: "",
+  })
+});
 </script>
 
 <template>
@@ -44,6 +67,19 @@ const userId = route.params.id
           <p class="font-bold text-gray-700">Seguidores</p>
         </div>
       </div>
+
+        <button
+            ref="referenceRef"
+            class="bg-rose-600 hover:bg-rose-500 text-white font-medium p-2.5 rounded-md leading-none">Click me</button>
+        <div
+            ref="floatingRef"
+            class="absolute top-0 left-0 z-50
+         bg-gray-700 text-sm text-white px-3 py-1.5 rounded-md cursor-default">Esto es un botón
+
+          <div class="bg-red-600 h-[8px] w-[8px] rotate-45" ref="arrowRef"></div>
+        </div>
+
+
     </div>
 
     <!-- Lateral Derecho -->
