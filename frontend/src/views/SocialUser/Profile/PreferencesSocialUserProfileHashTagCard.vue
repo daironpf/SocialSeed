cbae70a26d1b76d3a4125ba0cc49c448e69c9614
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2024-04-15 12:05:10 -0500

    Add ToolTip notifications

--- frontend/src/views/SocialUser/Profile/PreferencesSocialUserProfileHashTagCard.vue
+++ frontend/src/views/SocialUser/Profile/PreferencesSocialUserProfileHashTagCard.vue
@@ -1,8 +1,9 @@
 <script setup>
 const props = defineProps({
   hashtag: Object,
-
 })
+
+import BaseTooltip from "@/views/utils/BaseTooltip.vue";
 </script>
 
 <template>
@@ -10,9 +11,24 @@ const props = defineProps({
     <div class="">
       <p class="font-bold text-lg">#{{ hashtag.name }} </p>
     </div>
+
     <div class="flex items-center">
-      <p class="p-2"> <fa icon="fa-solid fa-pen-to-square text-blue-300" /> <span class="font-semibold">{{ hashtag.postTaggedIn }}</span> </p>
-      <p > <fa icon="fa-solid fa-user text-blue-300" /> <span class="font-semibold">{{ hashtag.socialUserInterestIn }}</span> </p>
+      <BaseTooltip
+          :content="`Total de publicaciones hechas con este HashTag: ${hashtag.postTaggedIn}`"
+          placement="bottom">
+        <p class="p-2">
+          <fa icon="fa-solid fa-pen-to-square text-blue-300" />
+          <span class="font-semibold">{{ hashtag.postTaggedIn }}</span>
+        </p>
+      </BaseTooltip>
+      <BaseTooltip
+          :content="`Total de Usuarios interesados en este HashTag: ${hashtag.socialUserInterestIn}`"
+          placement="bottom">
+      <p >
+        <fa icon="fa-solid fa-user text-blue-300" />
+        <span class="font-semibold">{{ hashtag.socialUserInterestIn }}</span>
+      </p>
+      </BaseTooltip>
     </div>
   </div>
 </template>
--- frontend/src/views/SocialUser/Profile/SocialUserProfileView.vue
+++ frontend/src/views/SocialUser/Profile/SocialUserProfileView.vue
@@ -1,42 +1,10 @@
 <script setup>
-
-import LeftSideView from "@/views/LeftSide/LeftSideView.vue";
 import RightSideView from "@/views/RightSide/RightSideView.vue";
-
 import { useRoute } from 'vue-router'
 import LeftSideSocialUserProfile from "@/views/SocialUser/Profile/LeftSideSocialUserProfile.vue";
-import ListOfFollowRecommendations from "@/views/Lists/ListOfFollowRecommendations.vue";
-import ListOfFriendsRecommendations from "@/views/Lists/ListOfFriendsRecommendations.vue";
-import {onMounted, ref} from "vue";
-import {arrow, computePosition, flip, offset, shift} from "@floating-ui/dom";
 
 const route = useRoute()
 const userId = route.params.id
-
-
-const referenceRef = ref();
-const floatingRef = ref();
-const arrowRef = ref();
-
-
-onMounted(async () => {
-  const {x, y, middlewareData, placement} = await computePosition(referenceRef.value, floatingRef.value, {
-    placement: "bottom",
-    middleware: [offset(8), flip(), shift({padding: 5}), arrow({element: arrowRef.value})]
-  });
-
-  Object.assign(floatingRef.value.style, {
-    left: `${x}px`,
-    top: `${y}px`,
-  });
-
-  const {x: arrowX, y: arrowY} = middlewareData.arrow;
-
-  Object.assign(arrowRef.value.style, {
-    left: arrowX ? `${arrowX}px`: "",
-    top: arrowY ? `${arrowY}px`: "",
-  })
-});
 </script>
 
 <template>
@@ -46,6 +14,7 @@ onMounted(async () => {
 
     <!-- Zona Central -->
     <div class="basis-1/2 bg-gray-200">
+
       <div class="basis-1/2 bg-gray-200 flex justify-around items-center">
         <!-- Botón de Publicaciones -->
         <div class="text-center">
@@ -67,19 +36,6 @@ onMounted(async () => {
           <p class="font-bold text-gray-700">Seguidores</p>
         </div>
       </div>
-
-        <button
-            ref="referenceRef"
-            class="bg-rose-600 hover:bg-rose-500 text-white font-medium p-2.5 rounded-md leading-none">Click me</button>
-        <div
-            ref="floatingRef"
-            class="absolute top-0 left-0 z-50
-         bg-gray-700 text-sm text-white px-3 py-1.5 rounded-md cursor-default">Esto es un botón
-
-          <div class="bg-red-600 h-[8px] w-[8px] rotate-45" ref="arrowRef"></div>
-        </div>
-
-
     </div>
 
     <!-- Lateral Derecho -->
