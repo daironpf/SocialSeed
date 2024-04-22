080623e76443e9db8a0c767a6f8f47e9e224522e
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2024-04-22 17:37:29 -0500

    add minor changes to buttoms

--- frontend/src/views/SocialUser/Profile/SocialUserProfileView.vue
+++ frontend/src/views/SocialUser/Profile/SocialUserProfileView.vue
@@ -19,7 +19,6 @@ const user = {
   posts: ['Publicación 1', 'Publicación 2', 'Publicación 3']
 };
 </script>
-
 <template>
   <div class="my-20 mx-3 flex flex-row">
     <!-- Latelar Izquierdo -->
@@ -28,29 +27,33 @@ const user = {
     <!-- Zona Central -->
     <div class="basis-1/2 bg-gray-200">
 
-      <div class="basis-1/2 bg-gray-200 flex justify-around items-center">
+      <div class="basis-1/2 bg-white mb-2 p-3 ml-10 mr-10 flex justify-around items-center">
         <!-- Botón de Publicaciones -->
-        <div class="text-center">
+        <div :class="{ 'active-tab': activeTab === 'posts' }" class="text-center relative">
           <p @click="activeTab = 'posts'"
-              class="font-bold text-gray-700">Publicaciones</p>
+             class="font-bold text-gray-700">Publicaciones</p>
+          <div v-if="activeTab === 'posts'" class="border-b-2 border-blue-500 absolute bottom-0 left-0 right-0"></div>
         </div>
 
         <!-- Botón de Amigos -->
-        <div class="text-center">
+        <div :class="{ 'active-tab': activeTab === 'friends' }" class="text-center relative">
           <p @click="activeTab = 'friends'"
              class="font-bold text-gray-700">Amigos</p>
+          <div v-if="activeTab === 'friends'" class="border-b-2 border-blue-500 absolute bottom-0 left-0 right-0"></div>
         </div>
 
         <!-- Botón de Seguidos -->
-        <div class="text-center">
+        <div :class="{ 'active-tab': activeTab === 'follows' }" class="text-center relative">
           <p @click="activeTab = 'follows'"
              class="font-bold text-gray-700">Seguidos</p>
+          <div v-if="activeTab === 'follows'" class="border-b-2 border-blue-500 absolute bottom-0 left-0 right-0"></div>
         </div>
 
         <!-- Botón de Seguidores -->
-        <div class="text-center">
+        <div :class="{ 'active-tab': activeTab === 'followers' }" class="text-center relative">
           <p @click="activeTab = 'followers'"
-              class="font-bold text-gray-700">Seguidores</p>
+             class="font-bold text-gray-700">Seguidores</p>
+          <div v-if="activeTab === 'followers'" class="border-b-2 border-blue-500 absolute bottom-0 left-0 right-0"></div>
         </div>
       </div>
 
@@ -80,5 +83,18 @@ const user = {
 </template>
 
 <style scoped>
+.active-tab {
+  color: #4CAF50; /* Color activo */
+}
 
+.active-tab:hover {
+  cursor: pointer;
+  opacity: 0.8; /* Reducir opacidad al pasar el mouse */
+}
+
+/* Estilo para el borde inferior */
+.border-b-2 {
+  border-bottom-width: 2px;
+}
 </style>
+
