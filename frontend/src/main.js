6a397d332bc1c54a704f286777216c99c4d9c040
Robe <roberto.granda@thecodelight.com>
2024-05-02 22:35:13 -0300

    feat: extract the app icons configuration into a file

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,44 +2,10 @@ import { createApp, provide } from 'vue'
 import App from './App.vue'
 import router from "@/router/index.js";
 
-/* import the fontawesome core */
-import { library } from '@fortawesome/fontawesome-svg-core';
-
-/* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
-/* import specific icons */
-import {
-    faBan,
-    faHome,
-    faUser,
-    faBell,
-    faEnvelope,
-    faCog,
-    faImage,
-    faClock,
-    faThumbsUp,
-    faComment,
-    faSync,
-    faPencil,
-    faEarth,
-    faSortDown,
-    faUserGroup,
-    faLock,
-    faUsers,
-    faUserFriends,
-    faUsersBetweenLines,
-    faUsersLine,
-    faUserPlus,
-    faPersonCirclePlus,
-    faPenToSquare
-} from "@fortawesome/free-solid-svg-icons";
-
-/* add icons to the library */
-library.add(faBan, faHome, faUser, faBell, faEnvelope, faCog, faImage, faClock, faThumbsUp, faComment, faSync, faPencil, faEarth, faSortDown, faUserGroup, faLock, faUsers, faUserFriends, faUsersBetweenLines, faUsersLine, faUserPlus, faPersonCirclePlus, faPenToSquare);
+import { FontAwesomeIcon } from '@/libs/app-icons.js';
 
 /* constant values */
-const api_url = "http://127.0.0.1:8081/api/v0.0.1/";
+const api_url = "http://54.183.246.185:8081/api/v0.0.1/";
 
 const app = createApp(App);
 app.use(router);
