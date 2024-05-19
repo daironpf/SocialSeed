c41d91796a2079446e94566d070fceb2bd155b29
Robe <roberto.granda@thecodelight.com>
2024-05-18 23:15:29 -0300

    feat: add infinity scroll component

---
+++ frontend/src/views/InfinityScroll.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {onMounted, ref} from 'vue';
+
+const props = defineProps({
+  class: String,
+  hasMoreElements: Boolean,
+})
+
+const LOAD_MORE_EVENT = 'LoadMoreElements';
+const emit = defineEmits([LOAD_MORE_EVENT])
+
+const targetElement = ref(null);
+
+onMounted(() => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log('INFO [InfinityScroll]: Intersection Observer is working!', { entry });
+      if (entry.isIntersecting && props.hasMoreElements) {
+        emit(LOAD_MORE_EVENT);
+      }
+    });
+  });
+
+  observer.observe(targetElement.value);
+});
+</script>
+
+<template>
+  <div :class="props.class">
+    <slot></slot>
+    <div ref="targetElement"></div>
+  </div>
+</template>
