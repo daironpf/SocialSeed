4412801aacb0f1dcf8681e91c11f7df730c53d01
Dairon Perez Frias <dairon.perezfrias@gmail.com>
2025-07-27 09:27:18 -0400

    : Proyecto reiniciado con arquitectura hexagonal y Spring Boot 4

---
+++ src/main/java/com/our/socialseed/SocialSeedApplication.java
@@ -0,0 +1,13 @@
+package com.our.socialseed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialSeedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/our/socialseed/SocialSeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.our.socialseed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialSeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
