239ac0026b3f4c70596d35404fbda5feb89cc8cf
Dairon Perez Frias <dairon.perezfrias@gmail.com>
2025-07-27 13:20:47 -0400

    Agragado spring security y configurado para desarrollo

---
+++ src/main/java/com/our/socialseed/config/DisableSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.our.socialseed.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+public class DisableSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+}
