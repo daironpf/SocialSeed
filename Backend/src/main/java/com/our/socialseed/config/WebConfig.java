1a222bf6e66bcba41240a5e7e8b961174513c185
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2025-09-07 16:25:24 -0500

    agregando CORS con permiso a todos los puertos de localhost para testear desde flutter solo para Dev

--- Backend/src/main/java/com/our/socialseed/config/WebConfig.java
+++ Backend/src/main/java/com/our/socialseed/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.our.socialseed.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,6 +11,20 @@ import java.nio.file.Paths;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    //Configuracion de CORS mientra se desarrolla
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*") // comodín para cualquier puerto
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
