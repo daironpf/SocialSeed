fbfa3a55be499570b919924a31f88c795190e3a9
Dairon Perez Frias <dairon.perezfrias@gmail.com>
2025-11-01 21:01:56 +0100

    init socialuser-service

---
+++ socialuser-service/src/main/java/com/socialseed/socialuserservice/SocialuserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.socialseed.socialuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialuserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialuserServiceApplication.class, args);
+    }
+
+}
