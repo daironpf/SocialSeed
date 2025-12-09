436bfb0289a9be56909cd6d70c4bf97da9bdc786
Dairon <dairon.perezfrias@gmail.com>
2025-12-09 15:21:48 -0500

    create reaction service

---
+++ reactions-service/src/main/java/com/socialseed/reactionsservice/ReactionsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.socialseed.reactionsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactionsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactionsServiceApplication.class, args);
+	}
+
+}
