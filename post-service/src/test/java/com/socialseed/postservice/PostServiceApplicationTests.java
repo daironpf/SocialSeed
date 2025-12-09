5f9a451dd95667df3d5bb7104da22232196b5c19
Dairon <dairon.perezfrias@gmail.com>
2025-12-09 14:56:15 -0500

    create post-service and add the old code

---
+++ post-service/src/test/java/com/socialseed/postservice/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialseed.postservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
