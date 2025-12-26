d49d2a56088e716bb11cc4a6933028f493f2b586
Dairon <dairon.perezfrias@gmail.com>
2025-12-25 21:57:55 -0500

    create governance-service

---
+++ governance-service/src/main/java/com/socialseed/governanceservice/GovernanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.socialseed.governanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovernanceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GovernanceServiceApplication.class, args);
+    }
+
+}
