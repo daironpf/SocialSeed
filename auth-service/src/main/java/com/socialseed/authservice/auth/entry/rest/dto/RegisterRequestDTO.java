046b2c9600b2c025f42c2a9c269a48dab6fd0984
Dairon <dairon.perezfrias@gmail.com>
2026-01-08 12:03:27 -0500

    determined use socialseed-platform as Git Subtree

--- auth-service/src/main/java/com/socialseed/authservice/auth/entry/rest/dto/RegisterRequestDTO.java
+++ auth-service/src/main/java/com/socialseed/authservice/auth/entry/rest/dto/RegisterRequestDTO.java
@@ -1,18 +1,17 @@
 package com.socialseed.authservice.auth.entry.rest.dto;
 
-import com.socialseed.authservice.platform.validation.annotation.ValidPassword;
-import com.socialseed.authservice.platform.validation.annotation.ValidUsername;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record RegisterRequestDTO (
-    @ValidUsername
+//    @ValidUsername
     String username,
 
     @NotBlank(message = "{user.email.required}")
     @Email(message = "{user.email.invalid}")
     String email,
 
-    @ValidPassword
+//    @ValidPassword
     String password
 ){}
